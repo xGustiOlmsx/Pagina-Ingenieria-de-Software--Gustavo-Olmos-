e2e866d8985e14a5a126fd724ded171a1015f63a
xGustiOlmsx <gustavoolmos002@gmail.com>
2022-06-15 15:52:58 -0300

    enter

---
+++ tabla/js/main.js
@@ -0,0 +1,13 @@
+function modoClaro() 
+{
+    var convertidor=document.getElementById("fondo");
+    convertidor.style.background="white";
+    convertidor.style.color="black";
+}
+
+function modoOscuro() 
+{
+    var convertidor=document.getElementById("fondo");
+    convertidor.style.background="black";
+    convertidor.style.color="white";
+}
