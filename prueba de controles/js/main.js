4a566eddacfe23dfcac4c445071f4bd6d220447a
xgustiolmsx <gustavoolmos002@gmail.com>
2022-06-22 18:40:40 -0300

    enter

---
+++ prueba de controles/js/main.js
@@ -0,0 +1,9 @@
+function cargarAyuda() {
+    var h3=document.getElementById(h3ayuda);
+    h3.innerHTML="Ingrese con mayúsculas su nombre";
+}
+
+function cargarAyuda2() {
+    var h3=document.getElementById(h3ayuda);
+    h3.innerHTML="Ingrese con mayúsculas su apellido";
+}
