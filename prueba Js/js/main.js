5ede090d59677dc70a06a11674f4ce7f5a9a867a
xGustiOlmsx <gustavoolmos002@gmail.com>
2022-06-21 22:54:16 -0300

    enter

---
+++ prueba Js/js/main.js
@@ -0,0 +1,13 @@
+function modoClaro() 
+{
+    var convertidor=document.getElementById("main");
+    convertidor.style.background="#ccc";
+    convertidor.style.color="#303841";
+}
+
+function modoOscuro() 
+{
+    var convertidor=document.getElementById("main");
+    convertidor.style.background="#303841";
+    convertidor.style.color="#ccc";
+}
