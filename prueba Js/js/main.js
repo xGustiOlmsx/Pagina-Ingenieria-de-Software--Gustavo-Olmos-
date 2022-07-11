6117556cc4321cb42810cf1ffc8b9645ba1742ee
xGustiOlmsx <gustavoolmos002@gmail.com>
2022-07-10 23:47:37 -0300

    enter

--- prueba Js/js/main.js
+++ prueba Js/js/main.js
@@ -1,15 +1,15 @@
-    alert("IMPORTANTE: esta es una maqueta simple, no esperes mucho ya que su única función es la de probar JavaScript en ella, gracias");
+alert("IMPORTANTE: esta es una maqueta simple, no esperes mucho ya que su única función es la de probar JavaScript en ella, gracias");    
 
 function modoClaro() 
 {
-    var convertidor=document.getElementById("main");
-    convertidor.style.background="#ccc";
-    convertidor.style.color="#303841";
+    var claro=document.getElementById("main");
+    claro.style.background="#ccc";
+    claro.style.color="#303841";
 }
 
 function modoOscuro() 
 {
-    var convertidor=document.getElementById("main");
-    convertidor.style.background="#303841";
-    convertidor.style.color="#ccc";
+    var oscuro=document.getElementById("main");
+    oscuro.style.background="#303841";
+    oscuro.style.color="#ccc";
 }
