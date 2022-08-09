30ad82b9a0e2b14b5eb0d39b6159b1875f05975f
xGustiOlmsx <gustavoolmos002@gmail.com>
2022-08-09 17:14:35 -0300

    nueva cara para la página

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
-function avisoPáginaEnProceso() {
-    swal("ATENCION:","Esta página todavía está en proceso, por favor seguí mirando las otras, gracias!!")
-}
+window.sr = ScrollReveal();
+
+    sr.reveal('.nav-list', {
+        duration: 1000,
+        origin: 'left',
+        distance: '-1000px'
+    });
+
+    sr.reveal('.projects-grid', {
+        duration: 1500,
+        origin: 'top',
+        distance: '-300px'
+    });
+
+    sr.reveal('.scroll-h1', {
+        duration: 1500,
+        origin: 'left',
+        distance: '-300px'
+    });
+
+    sr.reveal('.scroll-p', {
+        duration: 1500,
+        origin: 'right',
+        distance: '-300px'
+    });
+
+    sr.reveal('.contact-links', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+window.addEventListener("scroll", function(){
+    var nav = document.getElementById("navbar");
+    nav.classList.toggle("abajo",window.scrollY>0);
+})
