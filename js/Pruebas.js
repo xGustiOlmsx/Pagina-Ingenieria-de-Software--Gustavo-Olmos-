8ed26d9193c274d57f3bfc38feaff63f22720914
xGustiOlmsx <gustavoolmos002@gmail.com>
2022-08-09 23:10:46 -0300

    muchos, muchos, muchísimos arreglos y animaciones con javascript

--- js/Pruebas.js
+++ js/Pruebas.js
@@ -1,5 +1,3 @@
-alert("IMPORTANTE: esta es una maqueta simple, no esperes mucho ya que su única función es la de probar JavaScript en ella, gracias");    
-
 function modoClaro() 
 {
     var claro=document.getElementById("main");
@@ -13,3 +11,11 @@ function modoOscuro()
     oscuro.style.background="#303841";
     oscuro.style.color="#ccc";
 }
+
+window.sr = ScrollReveal();
+
+    sr.reveal('.nav-list', {
+        duration: 1000,
+        origin: 'left',
+        distance: '-1000px'
+    });
--- js/main.js
+++ js/main.js
@@ -1,36 +1,86 @@
 window.sr = ScrollReveal();
 
-    sr.reveal('.nav-list', {
-        duration: 1000,
-        origin: 'left',
-        distance: '-1000px'
+    sr.reveal('.nav-scroll', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px'
     });
 
     sr.reveal('.projects-grid', {
-        duration: 1500,
+        duration: 2000,
         origin: 'top',
         distance: '-300px'
     });
 
     sr.reveal('.scroll-h1', {
-        duration: 1500,
-        origin: 'left',
+        duration: 2000,
+        origin: 'top',
         distance: '-300px'
     });
 
     sr.reveal('.scroll-p', {
-        duration: 1500,
+        duration: 2000,
+        origin: 'top',
+        distance: '-300px'
+    });
+
+    sr.reveal('.scroll-h1a', {
+        duration: 2000,
+        origin: 'left',
+        distance: '-300px'
+    });
+
+    sr.reveal('.scroll-pa', {
+        duration: 2000,
         origin: 'right',
         distance: '-300px'
     });
 
-    sr.reveal('.contact-links', {
-        duration: 3000,
+    sr.reveal('.btn1', {
+        duration: 1000,
         origin: 'top',
-        distance: '-100px'
+        distance: '-300px'
+    });
+
+    sr.reveal('.btn2', {
+        duration: 1000,
+        origin: 'top',
+        distance: '-2000px'
+    });
+
+    sr.reveal('.btn3', {
+        duration: 1200,
+        origin: 'top',
+        distance: '-10000px'
+    });
+
+    sr.reveal('.btn4', {
+        duration: 1600,
+        origin: 'top',
+        distance: '-10000px'
     });
 
 window.addEventListener("scroll", function(){
     var nav = document.getElementById("navbar");
     nav.classList.toggle("abajo",window.scrollY>0);
 })
+
+barba.init({
+    transitions: [{
+        name: 'cambio',
+        leave: function(data){
+            var donde = this.async();
+            document.body.classList.add('loading');
+            setTimeout(function(){
+                done();
+            },900);
+        },
+        enter:
+        function(data){
+            var donde = this.async();
+            document.body.classList.add('loading');
+            setTimeout(function(){
+                done();
+            },900);
+    }}]
+});
